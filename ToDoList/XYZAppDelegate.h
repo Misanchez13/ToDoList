b97569c61cf53128d8627bd7b2f50086df1bdcad
Michael Sanchez <sanchez.soto.michael@gmail.com>
2014-09-15 23:32:30 -0600

    Prueba de iOS
    Aplicación de prueba para iOS

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Michael Sanchez Soto on 15/09/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
